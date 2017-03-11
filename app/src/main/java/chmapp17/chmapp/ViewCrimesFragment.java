6f727f61f3678a062c81a55d122abe196607e0c2
Edward <edward.mihailiuc@gmail.com>
2017-03-11 17:04:44 +0200

    Add map view

--- app/src/main/java/chmapp17/chmapp/ViewCrimesFragment.java
+++ app/src/main/java/chmapp17/chmapp/ViewCrimesFragment.java
@@ -1,27 +1,60 @@
 package chmapp17.chmapp;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.graphics.Color;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ViewCrimesFragment extends Fragment {
+public class ViewCrimesFragment extends Fragment implements OnMapReadyCallback {
 
-    public ViewCrimesFragment() {
-    }
+    private GoogleMap mMap;
+    private FragmentActivity myContext = new FragmentActivity();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_view_crimes, container, false);
     }
 
     @Override
     public void onStart(){
         super.onStart();
-        getView().setBackgroundColor(Color.MAGENTA);
+        SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        //getView().setBackgroundColor(Color.CYAN);
     }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+
 }
